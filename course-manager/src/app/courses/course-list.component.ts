23189e75108d71da81fd419c4eaffcf3823a9794
Cleison França Amorim <cleison.upgrade@gmail.com>
2020-02-24 14:00:26 -0400

    course angular

---
+++ course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+  selector: "app-course-list",
+  templateUrl: "course-list.component.html"
+})
+export class CourseListComponent implements OnInit {
+  courses: Course[] = [];
+
+  ngOnInit(): void {
+    this.courses = [
+      {
+        id: 1,
+        name: "Angular : forms",
+        imageUrl: "",
+        price: 99.99,
+        code: "sdr-456",
+        duration: 120,
+        rating: 5.4,
+        releaseDate: 'December 2019'
+      },
+      {
+        id: 2,
+        name: "Angular : http",
+        imageUrl: "",
+        price: 45.99,
+        code: "rtyr-56",
+        duration: 90,
+        rating: 4,
+        releaseDate: 'November 2019'
+      }
+    ];
+  }
+}
---
+++ course-manager/src/app/courses/course.ts
@@ -0,0 +1,10 @@
+export class Course{
+  id: Number;
+  name: String;
+  imageUrl: String;
+  price: Number;
+  code: String;
+  duration: Number;
+  rating: Number;
+  releaseDate: String;
+}
